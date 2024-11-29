243f7df7d21e8d70f86dc23aeb1efbace8733b96
PINIX1101 <fajartrisnopurwanto@yahoo.co.id>
2024-11-29 16:11:32 +0700

    [IMP] portal_purchase_custom: Fixing Float Logic

--- portal_purchase_custom/controllers/controllers.py
+++ portal_purchase_custom/controllers/controllers.py
@@ -9,14 +9,14 @@ class ContactUsContact(http.Controller):
         if kw:
             for line in range(int(kw.get('line_count'))):
                 id = int(kw.get('id' + str(line)))
-                product_qty = float(kw.get('product_qty' + str(line))) or 0.0
-                price_unit = float(kw.get('price_unit' + str(line))) or 0.0
-                discount = float(kw.get('discount' + str(line))) or 0.0
+                product_qty = kw.get('product_qty' + str(line)) or 0.0
+                price_unit = kw.get('price_unit' + str(line)) or 0.0
+                discount = kw.get('discount' + str(line)) or 0.0
                 purchase_line = request.env["purchase.order.line"].browse(id)
                 purchase_line.write({
-                    'product_qty': product_qty,
-                    'price_unit': price_unit,
-                    'discount': discount,
+                    'product_qty': float(product_qty),
+                    'price_unit': float(price_unit),
+                    'discount': float(discount),
                 })
             return request.redirect("/my/purchase/" + kw.get('order_id'))
         return request.redirect("/my/rfq")
